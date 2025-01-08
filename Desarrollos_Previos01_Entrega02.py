fa751e676292420e643dfb09382b020eb1df0acd
labravo20 <labravo@unal.edu.co>
2025-01-08 15:11:20 -0500

    Editando comentarios

--- Desarrollos_Previos01_Entrega02.py
+++ Desarrollos_Previos01_Entrega02.py
@@ -12,10 +12,19 @@ import matplotlib.pyplot as plt
 
 
 resolucion = 3000  # Número de puntos en la malla
-longitud_Arreglo = 0.5  # Tamaño físico del área (10 mm)
-radio = 0.002  # Radio del círculo en metros
+longitud_Arreglo = 0.5  # Tamaño físico del área 
+radio = 0.002  # Radio del círculo 
 centro = None  # El centro será el origen si es None
 
+
+""" Definición de distancias del arreglo """
+distancia_focal01 = 10
+distancia_focal02 = 10
+distancia_propagacion = 10
+
+
+
+""" Creando máscara de transmitancia asociada a abertura circular """
 
 # Crear la malla de puntos
 xx, yy = mascaras.malla_Puntos(resolucion, longitud_Arreglo)
@@ -23,9 +32,24 @@ xx, yy = mascaras.malla_Puntos(resolucion, longitud_Arreglo)
 # Crear la máscara circular
 mascara = mascaras.funcion_Circulo(radio, centro, xx, yy)
 
+
+
+
+""" Graficando máscara de transmitancia asignaada a abertura circular"""
+
 plt.imshow(mascara, extent=[-longitud_Arreglo/2, longitud_Arreglo/2, -longitud_Arreglo/2, longitud_Arreglo/2], cmap='gray')
 plt.title("Máscara Circular")
 plt.colorbar(label="Transmitancia")
 plt.xlabel("X (m)")
 plt.ylabel("Y (m)")
 plt.show()
+
+
+
+""" Aplicación de matrices de transmisión de rayos"""
+
+#Primer tramo del recorrido --> Propagación en distancia focal 01
+campo_propagadoMascara_lente01 = matriz.propagacion_MedioHomogeneo(distancia_focal01)*1
+
+#Segundo tramo del recorrido --> Paso a través de lente delgada
+campo_lente01 = 0
